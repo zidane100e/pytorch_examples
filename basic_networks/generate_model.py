a2003ca5c9744646efe92b1eba3f02694d8fd79e
bwlee <zidane100e@gmail.com>
2021-01-23 09:50:37 +0900

    aa

--- basic_networks/generate_model.py
+++ basic_networks/generate_model.py
@@ -10,8 +10,10 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader
-import sys
-sys.path.append('/home/bwlee/work/codes/examples/')
+
+import os, pathlib, sys
+module_dir = pathlib.Path(__file__).parent.parent.absolute()
+sys.path.append(str(module_dir))
 from kbutils.evaluation import accuracy
 
 def get_MLP(n_hiddens, activation=nn.ReLU(), dropout=0.1, end=False):
